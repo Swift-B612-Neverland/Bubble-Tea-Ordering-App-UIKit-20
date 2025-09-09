b3ef31d784ac11357f153273ab9255cae8f30e3f
Peter Pan <apppeterpan@gmail.com>
2025-09-09 14:02:32 +0800

    Initial Commit

---
+++ BubbleTeaOrderingAppUIKit20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BubbleTeaOrderingAppUIKit20
+//
+//  Created by SHIH-YING PAN on 2025/9/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
